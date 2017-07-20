70cd3c68f69da3238b957ba6825b2a41655ed502
Jeffrey Da <jexelD@gmail.com>
2017-07-19 17:44:55 -0700

    Before Apple Pay

---
+++ android/app/src/main/java/com/expo/xcodeapplepaytest2/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.expo.xcodeapplepaytest2.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
